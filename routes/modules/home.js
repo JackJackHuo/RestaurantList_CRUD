6d927922f107dd65b9a2e529217a39f5992ad2ea
JackHuo <cji4dk40@gmail.com>
2021-10-01 00:07:41 +0800

    feat: add name filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,8 +8,11 @@ const Restaurants = require('../../models/restaurants')
 const main = 'main'
 
 router.get('/', (req, res) => {
+  const sortType = req.query.sort
+  console.log(sortType)
   return Restaurants.find()
     .lean()
+    .sort({ name: sortType }) //在MongoDB將name屬性作正排序(asc)，反之(desc)
     .then(restaurants => {
       res.render('index', { restaurants: restaurants, stylesheet: main })
     })
